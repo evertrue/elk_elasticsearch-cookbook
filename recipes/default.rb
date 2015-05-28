82178b770e0c0c60c489ff69f43e6b23ef340d4b
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-05-28 14:28:10 -0400

    Move default recipe to install recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,20 +4,5 @@
 #
 # Copyright (c) 2015 EverTrue, inc., All Rights Reserved.
 
-include_recipe 'java'
 
-include_recipe 'storage'
-
-if node['storage']['ephemeral_mounts'].any?
-  node.set['elasticsearch']['path']['logs'] =
-    "#{node['storage']['ephemeral_mounts'].first}/elasticsearch/logs"
-  node.set['elasticsearch']['path']['data'] =
-    "#{node['storage']['ephemeral_mounts'].first}/elasticsearch/data"
-end
-
-node.set['elasticsearch']['node.master'] = node['roles'].include?('es_master')
-node.set['elasticsearch']['node.data'] = true
-
-include_recipe 'elasticsearch'
-include_recipe 'elasticsearch::plugins'
-include_recipe 'elasticsearch::search_discovery'
+include_recipe 'elk_elasticsearch::install'
---
+++ recipes/install.rb
@@ -0,0 +1,17 @@
+include_recipe 'java'
+
+include_recipe 'storage'
+
+if node['storage']['ephemeral_mounts'].any?
+  node.set['elasticsearch']['path']['logs'] =
+    "#{node['storage']['ephemeral_mounts'].first}/elasticsearch/logs"
+  node.set['elasticsearch']['path']['data'] =
+    "#{node['storage']['ephemeral_mounts'].first}/elasticsearch/data"
+end
+
+node.set['elasticsearch']['node.master'] = node['roles'].include?('es_master')
+node.set['elasticsearch']['node.data'] = true
+
+include_recipe 'elasticsearch'
+include_recipe 'elasticsearch::plugins'
+include_recipe 'elasticsearch::search_discovery'
